cdc7237778df44bbfd1a8f21eef9f4d573a1e111
Carlos Umaña Acevedo <carumtito@gmail.com>
2022-04-15 22:20:22 -0600

    Login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -9,41 +9,81 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     late String email, password;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
+    const gap = SizedBox(height: 30);
+
+    final decoration = InputDecoration(
+      filled: true,
+      isDense: true,
+      hintStyle: const TextStyle(fontSize: 13),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(10),
+        borderSide: BorderSide.none,
       ),
+    );
+
+    return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(15),
+        padding: const EdgeInsets.all(10.0),
         child: Column(
           children: [
-            TextField(
-              decoration: const InputDecoration(
-                labelText: 'Email',
+            const Spacer(),
+            const Image(
+              image: AssetImage('assets/logo.png'),
+              height: 60,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  TextField(
+                    decoration: decoration.copyWith(
+                      hintText: 'Phone number, username or email',
+                    ),
+                    onChanged: (value) => email = value,
+                  ),
+                  const SizedBox(height: 10),
+                  TextField(
+                    decoration: decoration.copyWith(
+                      hintText: 'Password',
+                    ),
+                    obscureText: true,
+                    onChanged: (value) => password = value,
+                  ),
+                  gap,
+                  const Text(
+                    'Forgot Password?',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                  gap,
+                  ElevatedButton(
+                    child: const Text('Log in'),
+                    onPressed: () => Auth().login(email: email, password: password),
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: const Size.fromWidth(double.maxFinite),
+                      primary: Colors.blue[800],
+                    ),
+                  ),
+                ],
               ),
-              onChanged: (value) => email = value,
             ),
-            TextField(
-              decoration: const InputDecoration(
-                labelText: 'Password',
-              ),
-              onChanged: (value) => password = value,
+            gap,
+            const Divider(thickness: 3),
+            gap,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Don\'t have an account?',
+                  style: TextStyle(color: Colors.grey),
+                ),
+                TextButton(
+                  child: const Text('Sign up'),
+                  onPressed: () => Navigator.pushNamed(context, REGISTER),
+                ),
+              ],
             ),
-            ElevatedButton(
-              child: const Text('Login'),
-              onPressed: () {
-                Auth().login(
-                  email: email,
-                  password: password,
-                );
-              },
-            ),
-            TextButton(
-              child: const Text('Register'),
-              onPressed: () {
-                Navigator.pushNamed(context, REGISTER);
-              },
-            )
+            const Spacer(),
           ],
         ),
       ),
