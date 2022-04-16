f2611875c16f4c5d32a9edd0350e37b63d4ab0fc
Carlos Umaña Acevedo <carumtito@gmail.com>
2022-04-15 22:51:39 -0600

    Search page

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/model/post_model.dart';
+import 'package:instagram_clone/provider/post_provider.dart';
+import 'package:instagram_clone/widgets/user_photo.dart';
+import 'package:provider/provider.dart';
 
 class SearchPage extends StatelessWidget {
   const SearchPage({Key? key}) : super(key: key);
@@ -6,12 +10,52 @@ class SearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Search'),
-        centerTitle: false,
-      ),
-      body: const Center(
-        child: Text('Search'),
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    isDense: true,
+                    hintText: 'Search',
+                    prefixIcon: const Icon(Icons.search, size: 25),
+                    hintStyle: const TextStyle(fontSize: 13),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: BorderSide.none,
+                    ),
+                    contentPadding: EdgeInsets.zero,
+                  ),
+                ),
+              ),
+            ),
+            StreamBuilder<List<PostModel>>(
+              stream: context.read<PostProvider>().postsStream,
+              initialData: context.read<PostProvider>().posts,
+              builder: (context, snapshot) {
+                final posts = snapshot.data!;
+
+                return SliverGrid(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    mainAxisSpacing: 3,
+                    crossAxisSpacing: 3,
+                  ),
+                  delegate: SliverChildBuilderDelegate(
+                    (_, index) => Image(
+                      image: NetworkImage(posts[index].imageUrl),
+                      fit: BoxFit.cover,
+                    ),
+                    childCount: posts.length,
+                  ),
+                );
+              },
+            )
+          ],
+        ),
       ),
     );
   }
