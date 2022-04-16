4263772c2da43dc31593501e19210260093915e0
Carlos Umaña Acevedo <carumtito@gmail.com>
2022-04-15 20:30:49 -0600

    Support web and macos

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:instagram_clone/provider/comment_provider.dart';
 import 'package:instagram_clone/provider/post_provider.dart';
 import 'package:instagram_clone/provider/user_provider.dart';
 import 'package:instagram_clone/routers/router_gerenator.dart';
@@ -10,6 +10,22 @@ import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyB8B7tt8gEhz8KLJVXke9gyD4wZ7hBS7JI",
+        authDomain: "instagram-clone-20a00.firebaseapp.com",
+        projectId: "instagram-clone-20a00",
+        storageBucket: "instagram-clone-20a00.appspot.com",
+        messagingSenderId: "1032309295781",
+        appId: "1:1032309295781:web:34861bd830d98b9cb55b4c",
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+
   await Firebase.initializeApp();
 
   final navigator = GlobalKey<NavigatorState>();
